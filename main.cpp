6d57a4b96676aaece158fbfee107fef90da5bdbf
CaolanCode <cmacmahon3@gmail.com>
2023-02-02 15:55:15 +0000

    commit files

---
+++ main.cpp
@@ -0,0 +1,107 @@
+/*
+    name: Caolan Mac Mahon
+    student number: C00222425
+    Lab 5
+ */
+#include <iostream>
+#include <time.h>
+#include <stdlib.h>
+#include <fstream>
+#include "questions.h"
+using std::cin;
+using std::string;
+using std::ofstream;
+
+int askQuestion();
+void storeScore(string name, int score);
+
+int main() {
+    
+    string name;
+    cout << "Enter your name: ";
+    getline(cin, name);
+    
+    int score = askQuestion();
+    
+    cout <<"\nScore: " << score << endl;
+    
+    storeScore(name, score);
+    
+    return 0;
+}
+
+int askQuestion()
+{
+    srand(time(0));
+    int score = 0;
+    char answer = 'Z';
+    char input = 'Z';
+    
+    while(input != 'Q')
+    {
+        int question = rand() % 10;
+        switch (question) {
+            case 0:
+                answer = question1();
+                break;
+            case 1:
+                answer = question2();
+                break;
+            case 2:
+                answer = question3();
+                break;
+            case 3:
+                answer = question4();
+                break;
+            case 4:
+                answer = question5();
+                break;
+            case 5:
+                answer = question6();
+                break;
+            case 6:
+                answer = question7();
+                break;
+            case 7:
+                answer = question8();
+                break;
+            case 8:
+                answer = question9();
+                break;
+            case 9:
+                answer = question10();
+                break;
+            default:
+                cout << "Error\n";
+                break;
+        }
+        cout << "\nAnswer (Q to Quit): ";
+        cin >> input;
+        input = toupper(input);
+        
+        if(input == 'Q') break;
+        
+        if(input == answer) {
+            cout << "\nCorrect!\n";
+            score++;
+        } else {
+            cout << "\nWrong!\n";
+        }
+    }
+    
+    return score;
+}
+
+void storeScore(string name, int score)
+{
+    ofstream scoreFile;
+    scoreFile.open("results.txt", std::ios_base::app);
+    
+    if(scoreFile.is_open()) {
+        scoreFile << name << "\n" << score << "\n\n";
+    } else {
+        cout << "Could not open file\n";
+    }
+    scoreFile.close();
+    
+}
---
+++ questions.h
@@ -0,0 +1,117 @@
+/*
+    Quiz Questions
+    Caolan Mac Mahon
+    C00222425
+ */
+using std::cout;
+using std::endl;
+
+char question1()
+{
+    cout << "\nIn which children’s book did a spider called Charlotte and a pig called Wilbur appear?\n\n";
+    cout << "A: Harry Potter\n";
+    cout << "B: Lord of the Rings\n";
+    cout << "C: Batman\n";
+    cout << "D: Charlotte's Web\n";
+    
+    return 'D';
+}
+
+char question2()
+{
+    cout << "\nWhat’s the biggest animal in the world?\n\n";
+    cout << "A: Rhino\n";
+    cout << "B: Blue Whale\n";
+    cout << "C: Elephant\n";
+    cout << "D: Hippo\n";
+    
+    return 'B';
+}
+
+char question3()
+{
+    cout << "\nHow many of Henry VIII’s wives were called Catherine?\n\n";
+    cout << "A: 4\n";
+    cout << "B: 7\n";
+    cout << "C: 3\n";
+    cout << "D: 2\n";
+    
+    return 'C';
+}
+
+char question4()
+{
+    cout << "\nWhat is the most popular baby boys name as of 2021?\n\n";
+    cout << "A: Martin\n";
+    cout << "B: Joseph\n";
+    cout << "C: Patrick\n";
+    cout << "D: Oliver\n";
+    
+    return 'D';
+}
+
+char question5()
+{
+    cout << "\nFrom which US city do the band The Killers originate?\n\n";
+    cout << "A: Las Vegas\n";
+    cout << "B: LA\n";
+    cout << "C: Washington\n";
+    cout << "D: New York\n";
+    
+    return 'A';
+}
+
+char question6()
+{
+    cout << "\nWhat is the name of the coffee shop in the US sitcom, Friends?\n\n";
+    cout << "A: Lemongrass\n";
+    cout << "B: Tea leaves\n";
+    cout << "C: Central Park\n";
+    cout << "D: After Eight\n";
+    
+    return 'C';
+}
+
+char question7()
+{
+    cout << "\nWho is currently (Oct 2022) the second in line to the throne?\n\n";
+    cout << "A: Prince William\n";
+    cout << "B: Prince Harry\n";
+    cout << "C: Prince George\n";
+    cout << "D: Prince Andrew\n";
+    
+    return 'C';
+}
+
+char question8()
+{
+    cout << "\nWhich planet has a pink sky?\n\n";
+    cout << "A: Mars\n";
+    cout << "B: Jupiter\n";
+    cout << "C: Saturn\n";
+    cout << "D: Mercury\n";
+    
+    return 'A';
+}
+
+char question9()
+{
+    cout << "\nWhat is the smallest planet in our solar system?\n\n";
+    cout << "A: Saturn\n";
+    cout << "B: Mercury\n";
+    cout << "C: Mars\n";
+    cout << "D: Jupiter\n";
+    
+    return 'B';
+}
+
+char question10()
+{
+    cout << "\nWhich fashion designer reportedly had a relationship with Winston Churchill?\n\n";
+    cout << "A: Rosalia Mera\n";
+    cout << "B: Coco Chanel\n";
+    cout << "C: Betsey Johnson\n";
+    cout << "D: Vivienne Westwood\n";
+    
+    return 'B';
+}
